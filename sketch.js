b9c249f66b3daace4c4d27cab0f0bd4920796b16
godzillaman <86501263+godzillaman@users.noreply.github.com>
2021-07-18 19:27:19 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var box1;
+var box2
+function setup() {
+  createCanvas(600, 400);
+  box1=new Box()
+  box2=new Box()
+}
+
+function draw() {
+  background(220);
+box1.show()
+box1.setWidth(100)
+box2.setWidth(200)
+box2.show()
+}
+
